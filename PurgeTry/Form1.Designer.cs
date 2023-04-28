f5266b313e53adabbf999d72a8e7974f84f7e9d0
Admin <zero21code@gmail.com>
2023-04-28 11:03:17 +0530

    Add project files.

---
+++ PurgeTry/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace PurgeTry
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Delete = new System.Windows.Forms.Button();
+            this.myLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Delete
+            // 
+            this.Delete.BackColor = System.Drawing.Color.IndianRed;
+            this.Delete.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Delete.Location = new System.Drawing.Point(19, 96);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(259, 85);
+            this.Delete.TabIndex = 0;
+            this.Delete.Text = "Delete";
+            this.Delete.UseVisualStyleBackColor = false;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // myLabel
+            // 
+            this.myLabel.Location = new System.Drawing.Point(65, 20);
+            this.myLabel.Name = "myLabel";
+            this.myLabel.Size = new System.Drawing.Size(154, 52);
+            this.myLabel.TabIndex = 1;
+            this.myLabel.Text = "Show\r\n";
+            this.myLabel.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 207);
+            this.Controls.Add(this.myLabel);
+            this.Controls.Add(this.Delete);
+            this.Name = "Form1";
+            this.Text = "Purge";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.Label myLabel;
+    }
+}
+
---
+++ PurgeTry/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace PurgeTry
+{
+    public partial class Form1 : Form
+    {
+        
+        string path = System.Configuration.ConfigurationSettings.AppSettings["Path"];
+        string year = "";
+        string month = "";
+        string days = "";
+        int processDay = Convert.ToInt32(System.Configuration.ConfigurationSettings.AppSettings["ProcessDays"]);
+              
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        public bool purgeFolderFiles()
+        {
+            bool complete = false;
+            DateTime dt = DateTime.Now.AddDays(-processDay);
+            int year = Convert.ToInt32(dt.Year);
+            string month = Convert.ToString(dt.Month);
+            if(month.Length == 1)
+            {
+                month = "0" + month;
+            }
+            string day = Convert.ToString(dt.Day);
+            if(day.Length == 1)
+            {
+                day = "0" + day;
+            }
+            string folderName = year + month + day;
+            if(Directory.GetDirectories(path).Length == 0)
+            {
+                return false;
+            }
+            foreach(string fil in Directory.GetDirectories(path)) 
+            {
+                try
+                {
+                    string filename = fil.Split('\\')[fil.Split('\\').Length - 1];
+                    filename = filename.Replace("-", "");
+                    if (Convert.ToInt32(filename) < Convert.ToInt32(folderName))
+                    {
+                        foreach (string fname in Directory.GetFiles(fil))
+                        {
+                            // 
+                            System.IO.File.Delete(fname);
+                        }
+
+                        Directory.Delete(fil);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+            return true;
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            bool complete = purgeFolderFiles();
+            if (complete)
+            {
+                myLabel.Visible = true;
+                myLabel.Text = "Done";
+            }
+            else
+            {
+                myLabel.Visible = true;
+                myLabel.Text = "File Not Found";
+            }
+            
+            
+        }
+
+
+
+        
+    }
+}
